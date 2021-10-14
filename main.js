407e1408bfaf161fc8948aaa7d196a78e43752d5
Lauren McCarthy <laurenleemccarthy@gmail.com>
2021-10-14 12:42:07 -0700

    working

--- main.js
+++ main.js
@@ -9,10 +9,29 @@ const images = [
   'assets/phrase5_@.png',
   'assets/phrase5_@.png'
 ];
+
+const alts = [
+  'These seats are reserved for',
+  'These seats are reserved for',
+  'These seats are reserved for',
+  'These seats are reserved for',
+  'These seats are reserved for',
+  'These seats are reserved for',
+  'These seats are reserved for',
+  'These seats are reserved for',
+  'These seats are reserved for',
+  'These seats are reserved for',
+  'These seats are reserved for',
+  'These seats are reserved for',
+  'These seats are reserved for'
+];
+
+
 const translations = {
-  title: ['I\'m glad you asked', '我很高兴你问'],
+  introTitle: ['I\'m\nglad\nyou\nasked', '我很高兴你问'],
   introEnter: ['Enter', '进入'],
   lang: ['中文', 'EN'],
+  helpTitle: ['I\'m glad you asked', '我很高兴你问'],
   helpDescript: ['I’m glad you asked spreads over Chinatown in San Francisco, augmenting the social landscape of the area. Various sites are virtually labeled with phrases such as “This seat is for people that need to be seen.” or “This seat is for people that are missing someone.” Visitors that identify with the statements may sit, inevitably mixing with unwitting visitors that are simply sitting. In the process of determining the other’s reasons for being there, spontaneous conversations may arise. “Excuse me, are you also missing someone?”',
                  '我很高兴你问了旧金山唐人街的分布，增加了该地区的社会景观。 各种网站都虚拟地贴上了诸如“这个座位是给需要被看到的人”之类的短语的标签。 或“这个座位是为想念某人的人准备的。” 认同这些陈述的访问者可能会坐着，不可避免地与只是坐着的不知情的访问者混在一起。 在确定对方在场原因的过程中，可能会出现自发的对话。 “请问，您也想念一个人吗？”'],
   artist: ['Lauren Lee McCarthy', '劳伦·李·麦卡锡'],
@@ -21,31 +40,37 @@ const translations = {
 
 
 let lang = 'en';
+let camera;
 
 $('#enter-button').click(enter);
 $('#help-button').click(toggleHelp);
 $('#lang-button').click(changeLang)
+$( window ).bind('hashchange', init);
+$( window ).resize(resizeCam);
+
 resize();
 init();
 
-function resize() {
-  let vw = window.innerWidth * 0.01;
-  document.documentElement.style.setProperty('--vw', `${vw}px`);
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+function enter() {
+  $('#enter-button').text('Loading');
+  $('#enter-button').addClass('fade');
+  $('#enter-button').prop('disabled', true);
+  startCam();
+
 }
 
-function enter() {
-  
+function startCam() {
   let videoElement = document.getElementById('cam');
-  let camera = new JslibHtml5CameraPhoto.default(videoElement);
-  camera.startCamera(JslibHtml5CameraPhoto.FACING_MODES.ENVIRONMENT)
+  camera = new JslibHtml5CameraPhoto.default(videoElement);
+  camera.startCameraMaxResolution(JslibHtml5CameraPhoto.FACING_MODES.ENVIRONMENT)
   .then((stream)=>{
     console.log('Camera started');
-    showMain();
+    resizeCam();
+    videoElement.addEventListener('playing', showMain);
   })
   .catch((error)=>{
     console.log('Camera failed');
+    $('#error').show();
   });
 }
 
@@ -80,6 +105,7 @@ function init() {
   console.log(n);
   let path = images[n].replace('@', lang);
   $('#phrase').attr('src', path);
+  $('#phrase').attr('alt', alts[n]);
   $('#debug').html(n);
   initLang();
 }
@@ -92,6 +118,31 @@ function initLang() {
   });
 }
 
-window.onhashchange = function() {
-  init();
+function resize() {
+  let vw = window.innerWidth * 0.01;
+  document.documentElement.style.setProperty('--vw', `${vw}px`);
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+function resizeCam() {
+  if (!camera) return;
+  let cameraSettings = camera.getCameraSettings();
+  if (cameraSettings) {
+    let ww = window.innerWidth, wh = window.innerHeight;
+    let ca = cameraSettings.aspectRatio, cw = cameraSettings.width, ch = cameraSettings.height;
+    if (cw / ch > ww / wh) {
+      $('#cam').height(wh);
+      $('#cam').width(ca * wh);
+      $('#cam').css('top', 0);
+      $('#cam').css('left', -0.5 * (cw - ww));
+
+    } else {
+      $('#cam').width(ww);
+      $('#cam').height(ww / ca);
+      $('#cam').css('top', -0.5 * (ch - wh));
+      $('#cam').css('left', 0);
+    }
+    console.log(cw, ch, ca, ww, wh)
+  }
 }
